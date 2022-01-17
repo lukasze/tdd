42e50870353532f2581fe025e83deaa7e672d384
lukasze <wyspianski.lukasz@gmail.com>
2022-01-17 11:16:11 +0100

    Pierwszy test z MockMvc: POST /hello -> 405

--- src/test/java/com/example/hellotest/controller/HelloControllerTest.java
+++ src/test/java/com/example/hellotest/controller/HelloControllerTest.java
@@ -1,17 +1,21 @@
 package com.example.hellotest.controller;
 
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 // Podnieś cały kontekst Spring'a przed testem
 @SpringBootTest
-// Spring - Skonfiguruj mi ziarno MockMvc (
+// Spring - Skonfiguruj mi ziarno MockMvc
 @AutoConfigureMockMvc
 class HelloControllerTest {
 
@@ -39,8 +43,17 @@ class HelloControllerTest {
 
     @Test
     @DisplayName("POST /hello -> 405")
-    void helloUsingPOST() {
-        fail();
+    void helloUsingPOST() throws Exception {
+        // given: /hello (GET)
+
+        // when
+        mockMvc
+                // wyślij żądanie POST /hello
+                .perform(post("/hello"))
+                // lepsze logowanie
+                .andDo(print())
+        // then
+                .andExpect(MockMvcResultMatchers.status().is(405));
     }
 
     @Test
