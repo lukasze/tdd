895159ff61516e9c5306617c862b626e66397bfe
lukasze <wyspianski.lukasz@gmail.com>
2022-01-17 11:26:00 +0100

    Refactor: /hello zwraca obiekt Response zamiast String'a

--- src/main/java/com/example/hellotest/controller/HelloController.java
+++ src/main/java/com/example/hellotest/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.hellotest.controller;
 
+import com.example.hellotest.model.Response;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${spring.application.name}")
+    private String service;
+
     @GetMapping("/hello")
-    String greet(@RequestParam(required = false, defaultValue = "World") String name) {
-        return "Hello, " + name + "!";
+    Response greet(@RequestParam(required = false, defaultValue = "World") String name) {
+        var message=  "Hello, " + name + "!";
+        return new Response(message, service);
     }
 }
