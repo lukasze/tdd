f4df2f302515adfcadeca2b3f9e24970d1e190b9
lukasze <wyspianski.lukasz@gmail.com>
2022-01-17 10:29:22 +0100

    Inicjalizacja projektu: Web, Spring Data Jpa, Lombok, H2

---
+++ src/test/java/com/example/hellotest/HelloTestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.hellotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+class HelloTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
